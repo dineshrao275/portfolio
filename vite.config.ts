b96451bca657868667c8a18c04ea629d398c2029
dineshrao275 <dineshrao275@gmail.com>
2025-04-21 16:02:16 +0530

    Update Vite configuration to ensure base path matches repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio", //  Add this line
+  base: "/portfolio/", // 👈 this MUST match your repo name
   server: {
     host: "::",
     port: 8080,
