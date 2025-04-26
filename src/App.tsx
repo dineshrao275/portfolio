463ece9e34491c9fa7f394d14ad0663e7d048c3d
dineshrao275 <dineshrao275@gmail.com>
2025-04-26 23:20:36 +0530

    Update homepage URL in package.json and adjust BrowserRouter basename in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/portfolio">
+      {/* <BrowserRouter basename="/portfolio"> */}
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
