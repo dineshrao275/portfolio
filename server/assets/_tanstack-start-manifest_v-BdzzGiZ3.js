ea41b9890cd70af7104a932cd77e6e97a12c208a
dineshrao275 <dineshrao275@gmail.com>
2026-06-10 01:09:58 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-BdzzGiZ3.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/drao/Downloads/portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DLM1czL3.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-DLM1czL3.js" } }] }, "/": { filePath: "/home/drao/Downloads/portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Dby6Zjk1.js"] } } });
-export {
-  tsrStartManifest
-};
